839bbf3b0a1b78a95c9c258a83acff0cbafd440b
NehharShah <nehharshah@gmail.com>
2025-08-06 17:18:13 -0400

    vercel deployment-14

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
   images: {
     domains: ['via.placeholder.com'],
+    unoptimized: true,
   },
   env: {
     NEXT_PUBLIC_VAULT_ADDRESS: process.env.NEXT_PUBLIC_VAULT_ADDRESS || '0x9094E827F56c1a19666B9D33790bFf0678868685',
